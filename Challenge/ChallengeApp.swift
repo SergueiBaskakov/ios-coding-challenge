b080a38b1077c5fe8536def6cfe4e39b2bd6c77a
Serguei <serguei.diaz@whiz.one>
2021-10-10 12:50:09 -0500

    Initial Commit

---
+++ Challenge/ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeApp.swift
+//  Challenge
+//
+//  Created by Serguei on 10/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
